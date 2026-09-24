384ee307d46d8854d78d2705b46963a46284a4b3
dwivedishivani <shivanidwivedi230797@gmail.com>
2026-09-24 06:21:01 +0530

    Clarify public Supabase key naming

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -3,4 +3,4 @@
  * Never place a Supabase secret/service-role key in this file.
  */
 export const projectId = "etmrhxapfvyaispuvnft"
-export const publicAnonKey = "sb_publishable_ShsF8PZILAPoC18jC9wuXA_Y_yH78bs"
+export const publicSupabaseKey = "sb_publishable_ShsF8PZILAPoC18jC9wuXA_Y_yH78bs"
